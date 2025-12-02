8b244c2a28bd1bd85187e9e792f015436736bdc0
yanzexuan <yanzexuan@xiaohongshu.com>
2025-12-02 11:40:19 +0800

    stop tracking run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-
-# 2. 运行 Paper2Video (指定 model_name 为 azure-4o)
-python src/pipeline.py \
-  --model_name_t azure-4o \
-  --model_name_v azure-4o \
-  --paper_latex_root ./assets/demo/latex_proj \
-  --ref_img ./assets/demo/zeyu.png \
-  --ref_audio ./assets/demo/zeyu.wav
